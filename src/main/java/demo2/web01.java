4ca33f25c4c9548f5794ebc0f5f34c7817719679
huangxinyue123 <2359963038@qq.com>
2020-09-01 16:37:14 +0800

    本次提交的信息

---
+++ src/main/java/demo2/web01.java
@@ -0,0 +1,24 @@
+package demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication // Spring Boot项目的核心注解，主要目的是开启自动配置
+@Controller // 标明这是一个SpringMVC的Controller控制器
+public class web01 {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+    }
+
+    // 在main方法中启动一个应用，即：这个应用的入口
+    public static void main(String[] args) {
+        SpringApplication.run(web01.class, args);
+    }
+
+}
